a6c48e6d1fafd359e3a3147ab6062031ef82fe92
Jose Clemente <jose_clemente_corzo@hotmail.com>
2024-10-11 19:18:47 -0600

    Conexió a Base de Datos

--- Practica 04_python/Practica_04_python.py
+++ Practica 04_python/Practica_04_python.py
@@ -1,20 +1,68 @@
+import re
 import tkinter as tk 
 from tkinter import messagebox
+import mysql.connector
 
 ventana = tk.Tk()
 ventana.geometry("500x500")
 ventana.title("Datos de registro")
 var_genero = tk.IntVar()
 
+def insertarRegristro(nombre, apellido, edad, estatura, telefono, genero):
+    try:
+        conexion = mysql.connector.connect(
+            host="localhost",
+            port ="3306",
+            user="root",
+            password="",
+            database ="Registros_Avanzada"
+            )
+        cursor = conexion.cursor()
+        query = "INSERT INTO Registros (nombre, apellido, edad, estatura, telefono, genero) VALUES (%s, %s, %s, %s, %s, %s)"
+        valores = (nombre, apellido, edad, estatura, telefono, genero)
+        cursor.execute(query, valores)
+        conexion.commit()
+        #cerrar la conexion a base de datos
+        cursor.close()
+        conexion.close()
+        messagebox.showinfo("Informacion", "Datos guardados correctamente") 
+    except mysql.connector.Error as err:
+        messagebox.showerror("Error",f"Error al insertar datos en la base de datos {err}")
+
+        
 def limpiar_campos():
     tbNombre.delete(0, tk.END)
     tbApellido.delete(0, tk.END)
     tbEdad.delete(0, tk.END)
     tbEstatura.delete(0, tk.END)
+    tbTelefono.delete(0, tk.END)
     var_genero.set(0)
+    
 
 def borrar():
     limpiar_campos()
+    
+def TelefonoValido(valor):
+    return valor.isdigit() and len(valor)==10 
+
+def TextoValido(valor):
+    return bool(re.match("^[a-zA-Z\s]+$", valor))
+
+def EnteroValido(valor):
+    try:
+        int(valor)
+        return True
+    except ValueError:
+        return False
+    
+def DecimalValido(valor):
+    try:
+        float(valor)
+        return True
+    except ValueError:
+        return False
+
+
     
 def guardar():
     nombre = tbNombre.get()
@@ -28,13 +76,21 @@ def guardar():
         genero="Hombre"
     elif var_genero.get()==2:
         genero="Mujer"
-        
-    datos = "Nombres: " + nombre + "\n" + "Apellido: " + apellido + "\n"  + "Edad: " + edad + "\n" + "Telefono:  " + telefono + "\n" + "Estatura: " + estatura + "\n" + "Genero: " + genero + "\n" 
-    with open ("C:\\Users\\Ruben Clemente\\Documents\\Practica 04\\Practica 04.txt", "a") as archivo:
+
+    if (EnteroValido(edad) and DecimalValido(estatura) and TelefonoValido(telefono) and TextoValido(nombre)):
+    
+        datos = "Nombres: " + nombre + "\n" + "Apellido: " + apellido + "\n"  + "Edad: " + edad + "\n" + "Telefono:  " + telefono + "\n" + "Estatura: " + estatura + "\n" + "Genero: " + genero + "\n" 
+        with open ("C:\\Users\\Ruben Clemente\\Documents\\Practica 04\\Practica 04.txt", "a") as archivo:
             archivo.write(datos + "\n\n")
             
-    messagebox.showinfo("Informacion", "Datos guardados correctamente: \n\n" + datos)
+        messagebox.showinfo("Informacion", "Datos guardados correctamente: \n\n" + datos)
+        insertarRegristro(nombre, apellido, edad, estatura, telefono, genero)
+        borrar()
+   
+    else:
+        messagebox.showerror("Error", "Algunos campos son incorrectos")
 
+        
 
 lbNombre = tk.Label(ventana, text="Nombre")
 lbNombre.pack()
@@ -74,4 +130,4 @@ bnBorrar.pack()
 bnGuardar = tk.Button(ventana, text="Guardar", command=guardar)
 bnGuardar.pack()
 
-ventana.mainloop()
+ventana.mainloop() 
